540a607d4243594a0b36713b4764262ee5cd5619
Diego <diegors90@gmail.com>
2025-05-14 22:10:33 -0300

    unnamed variables and simple files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
-package org.example;
+void main() {
+    println("Hello...");
+    String name = readln();
+    System.out.println("name: " + name);
 
-public class Main {
-    public static void main() {
-        System.out.println("Hello...");
+    unnamedVariables();
+}
+
+
+void unnamedVariables() {
+
+    try{
+        Integer.parseInt("teste");
+    } catch (NumberFormatException _) {
+        System.out.println("error...");
     }
+
+    IntStream.range(0, 5).forEach(_ -> System.out.println("unnamed"));
 }
